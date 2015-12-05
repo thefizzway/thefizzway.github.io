12b5fd0ba52baadb958176cd322bc5239e10f39b
Vinit Jogani <vnjogani@gmail.com>
2015-12-05 16:23:30 +0530

    unit 1 changes
    corrected unit 1

--- js/directives/image.js
+++ js/directives/image.js
@@ -1,10 +1,15 @@
-app.directive("blockImage", function(){
+app.directive("blockImage", function($window){
   return {
    restrict:"E",
    templateUrl:"js/templates/image.html",
+    controller: function($scope, $window) {
+       $scope.window = function(src) {
+           $window.open(src);
+       }
+    },
    scope:
    {
-     src:'@source'
+        src:'@source'
    }
   };
 });
